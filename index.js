03667eaeaaa30facc47725f3b99def7bc9c498f8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-29 11:04:52 -0400

    Adds 'name' to initial state and creates action to change it

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const { createStore } = require('redux');
 
 const AUMENTAR_CONTADOR = 'AUMENTAR_CONTADOR';
 const DECREMENTAR_CONTADOR = 'DECREMENTAR_CONTADOR';
+const CAMBIAR_NOMBRE = 'CAMBIAR_NOMBRE';
 
 const initialState = {
-    count: 0, 
-    users: [],
-    friends: {},
-    darkMode: true,
+    count: 0,
+    name: 'Jorge', 
 };
   
 const reducer = (state = initialState, action) => {
@@ -16,6 +15,8 @@ const reducer = (state = initialState, action) => {
             return {...state, count: state.count + 1};
         case DECREMENTAR_CONTADOR:
             return {...state, count: state.count - 1};
+        case CAMBIAR_NOMBRE:
+            return {...state, name: action.payload};    
         default:
             return {...state};
     }
@@ -30,10 +31,5 @@ const print = () => {
 store.subscribe(print);
 
 store.dispatch({type: AUMENTAR_CONTADOR});
-store.dispatch({type: AUMENTAR_CONTADOR});
-store.dispatch({type: AUMENTAR_CONTADOR});
 store.dispatch({type: DECREMENTAR_CONTADOR});
-store.dispatch({type: DECREMENTAR_CONTADOR});
-store.dispatch({type: DECREMENTAR_CONTADOR});
-store.dispatch({type: AUMENTAR_CONTADOR});
-store.dispatch({type: DECREMENTAR_CONTADOR});
+store.dispatch({type: CAMBIAR_NOMBRE, payload: 'Juan'});
