df784517fa41cb113e09f38e135d5cc8e5b8368b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-28 20:40:46 -0400

    Finish the reducer

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const initialState = {
   count: 0
 };
 
-const reducer = (state, action) => {};
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'AUMENTAR_CONTADOR':
+            return {...state, count: state.count + 1};
+        default:
+            return {...state};
+    }
+};
 
-const store = createStore();
+const store = createStore(reducer);
 
